2e39c6359674a2f3aba619611e4bf7d25ce378ba
osvaldo <71652778+devOsvaldo2020@users.noreply.github.com>
2022-02-22 13:54:32 -0300

    att

--- botoes/selecionadoAcende/js/scriptTest5.js
+++ botoes/selecionadoAcende/js/scriptTest5.js
@@ -1,8 +1,8 @@
 const allButons = document.querySelectorAll("input"); // pega do html todos os inputs/botoes
 
 allButons.forEach(button => button.addEventListener('click', (e) => {
-    clearActiveButton();
-    e.target.classList.add('active');
+    clearActiveButton(); // func apagar a class active
+    e.target.classList.add('active'); // evento de add a class active
 }));
 
 const clearActiveButton = () => allButons.forEach(button => button.classList.remove('active'));
